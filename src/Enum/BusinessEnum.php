3785df7359b32e3f76418cf4dec8b1d4249386e9
qimengyun <qimengyun2020@163.com>
2024-08-08 14:45:23 +0800

    2024年8月8日14:45:22

---
+++ src/Enum/BusinessEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mine\Enum;
+
+enum BusinessEnum
+{
+    //用户端
+    const CLIENT = 'client';
+    //商户端
+    const MERCHANT = 'merchant';
+    //平台
+    const PLATFORM = 'platform';
+}
