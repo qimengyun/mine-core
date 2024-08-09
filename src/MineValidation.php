d5b89eb026d10b635fa6d013f1e56ece9d1219bd
qimengyun <qimengyun2020@163.com>
2024-08-09 11:27:17 +0800

    2024年8月9日11:27:15

--- src/MineValidation.php
+++ src/MineValidation.php
@@ -10,6 +10,7 @@ class MineValidation
     private array $scenes = [];
     private array $messages = [];
     private array $customAttributes = [];
+    private array $methods = [];
     private string $errors;
     private bool $status;
     private array $data;
@@ -21,6 +22,7 @@ class MineValidation
 
     private function getValidation($validation): void
     {
+        $this->methods = get_class_methods($validation);
         $argv = get_class_vars($validation);
         if ($argv) {
             $this->scenes = $argv['scenes'] ?? [];
@@ -36,7 +38,7 @@ class MineValidation
         $rules = $this->rules;
         if (!($rules && $data)) {
             $this->errors = 'missing parameter';
-            return true;
+            $this->status = true;
         }
         if ($scene && $this->scenes) {
             $scenes = $this->scenes;
@@ -50,13 +52,55 @@ class MineValidation
                 }
             }
         }
+        $custom_rules = [];
+        if ($this->methods) {
+            $methods = $this->methods;
+            foreach ($rules as $key => $value) {
+                $arr = explode('|', $value);
+                foreach ($arr as $k => $method) {
+                    if (in_array($method, $methods, true)) {
+                        $custom_rules[] = [
+                            'method' => $method,
+                            'value'  => $data[$key],
+                            'data'   => $data
+                        ];
+                        unset($arr[$k]);
+                    }
+                }
+                $rules[$key] = implode('|', $arr);
+            }
+        }
+
         $validator = $this->validationFactory->make($data, $rules, $this->messages, $this->customAttributes);
         if ($validator->fails()) {
             $this->errors = $validator->errors()->first();
             $this->status = true;
         } else {
-            $this->data = $data;
-            $this->status = false;
+            if ($custom_rules) {
+                $validator = new $validation;
+                foreach ($custom_rules as $rules) {
+                    $method = $rules['method'];
+                    $result = $validator->$method($rules['value'], $rules['data']);
+                    if (is_bool($result)) {
+                        if (!$result) {
+                            $this->errors = 'tc:common.error';
+                            $this->status = true;
+                            return true;
+                        }
+                    } else {
+                        $this->errors = $result;
+                        $this->status = true;
+                        return true;
+                    }
+                }
+                $this->data = $data;
+                $this->status = false;
+                return false;
+            } else {
+                $this->data = $data;
+                $this->status = false;
+                return false;
+            }
         }
     }
 
