48b687e36b42cecccfb1b832b806e38b07cbcb07
qimengyun <qimengyun2020@163.com>
2024-08-14 10:11:07 +0800

    2024年8月14日10:11:06

--- src/MineModel.php
+++ src/MineModel.php
@@ -13,81 +13,8 @@ declare(strict_types=1);
 namespace Mine;
 
 use Hyperf\DbConnection\Model\Model;
-use Hyperf\ModelCache\Cacheable;
 
 
 class MineModel extends Model
 {
-    use Cacheable;
-
-    /**
-     * 状态
-     */
-    public const ENABLE = 1;
-
-    public const DISABLE = 2;
-
-    /**
-     * 默认每页记录数.
-     */
-    public const PAGE_SIZE = 15;
-
-    /**
-     * 隐藏的字段列表.
-     * @var string[]
-     */
-    protected array $hidden = ['deleted_at'];
-
-    /**
-     * 数据权限字段，表中需要有此字段.
-     */
-    protected string $dataScopeField = 'created_by';
-
-    /**
-     * MineModel constructor.
-     */
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
-
-        // 注册常用方法
-        $this->registerBase();
-        // 注册用户数据权限方法
-        $this->registerUserDataScope();
-    }
-
-    /**
-     * 设置主键的值
-     * @param int|string $value
-     */
-    public function setPrimaryKeyValue($value): void
-    {
-        $this->{$this->primaryKey} = $value;
-    }
-
-    public function getPrimaryKeyType(): string
-    {
-        return $this->keyType;
-    }
-
-    public function save(array $options = []): bool
-    {
-        return parent::save($options);
-    }
-
-    public function update(array $attributes = [], array $options = []): bool
-    {
-        return parent::update($attributes, $options);
-    }
-
-    public function getDataScopeField(): string
-    {
-        return $this->dataScopeField;
-    }
-
-    public function setDataScopeField(string $name): self
-    {
-        $this->dataScopeField = $name;
-        return $this;
-    }
 }
